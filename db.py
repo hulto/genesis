26c0b2071a2b0d1fd2609286b3b639139e25fafe
Hulto <jack.m.mckenna@gmail.com>
2019-06-20 01:01:49 -0400

    Fixed tag

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import sessionmaker
@@ -11,20 +11,42 @@ import types
 
 engine = create_engine('sqlite:////tmp/test.db', echo=True)
 Base = declarative_base()
-
 class Tag(Base):
+    __tablename__ = 'tag'
+
     id = Column(Integer, primary_key=True)
-    parentId = Column(Integer, ForeignKey('tag.id'))
     hosts = None
     name = 'default'
-    def __init__(self, name: str, parent=None, children=[], hosts=[]):
-        self.name = name
-        self.parent = parent
-        if parent is not None:
-            self.parent.addChild(self)
-        self.children = []
-        self.hosts = hosts
+    
+    parent_id = Column(Integer, ForeignKey('tag.id'))
+    parent = relationship("Tag")
 
+#    parentId = Column(Integer, ForeignKey('Tag.id'), index=True)
+#    parent = relationship('Tag', backref=backref('parent', remote_side='Tag.id'))
+
+
+    def __init__(self, name: str, parent=None, hosts=[]):
+        self.name = name
+        self.hosts = hosts
+        if parent is not None:
+            self.parent_id = parent.id
+        else:
+            self.parent_id = -1
+
+        session.add(self)
+        try:
+            session.commit()
+        except exc.IntegrityError as err:
+            session.rollback()
+            if "UNIQUE constraint failed: projects.name" in str(err):
+                sys.stderr.write("error, project name already exists (%s)\n" % name)
+            elif "FOREIGN KEY constraint failed" in str(err):
+                sys.stderr.write("supplier does not exist\n")
+            else:
+                sys.stderr.write("unknown error project user")
+
+    def __repr__(self):
+        return("<Tag(name='%s', parent_id='%d')>" % (self.name, self.parent_id))
 
 class User(Base):
     __tablename__ = 'users'
@@ -83,6 +105,13 @@ Session = sessionmaker(bind=engine)
 session = Session()
 if __name__ == "__main__":
     print("Starting...") 
+    mytag = Tag("all")
+    linuxTag = Tag("linux", mytag)
+    linuxTag = Tag("windows", mytag)
+    linuxTag = Tag("bsd", mytag)
+
+    print(session.query(Tag).all())
+    #mytag.child = Tag("linux")
     #myuser = User("Jack", "Jack McKenna", "Hulto")
     #myproject = Project("budlight", "private", myuser.id)
     #myproject = Project("opta", "public", myuser.id)
@@ -90,9 +119,9 @@ if __name__ == "__main__":
     #print("HERE")
     #print(session.query(User).all())
     #print(session.query(Project).all())
-    userId = int(session.query(User.id).filter_by(nickname="Hulto").all()[0][0])
-    for i in session.query(Project).filter_by(ownerId=userId).all():
-        print(repr(i))
+    #userId = int(session.query(User.id).filter_by(nickname="Hulto").all()[0][0])
+    #for i in session.query(Project).filter_by(ownerId=userId).all():
+    #    print(repr(i))
 #    print(session.query(Project).filter_by(ownerId=myuser.id).all())
     #print(session.query(Project).filter_by(ownerId=myuser.id).all())
     
